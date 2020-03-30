1bca29ffecb73de64e3b12956acbe01532340fe8
Lorna Kimani <kimanilorna@yahoo.com>
2020-03-30 18:41:33 +0300

    update url for fetching pending policies

--- src/components/life_policies/life_policies_pending.vue
+++ src/components/life_policies/life_policies_pending.vue
@@ -3,7 +3,7 @@
     <div class="page-table scrollable">
         <h1 class="animated fadeInDown">Life Policies Pending</h1>
         <el-row id="space">
-            <el-button  type="primary" v-on:click="addClicked" icon="el-icon-plus">Add New</el-button>
+            <!-- <el-button  type="primary" v-on:click="addClicked" icon="el-icon-plus">Add New</el-button> -->
             <el-button type="success" icon="el-icon-info" v-on:click="exportXlsx">Export Excel</el-button>
         </el-row>
         <div class="table-box card-base card-shadow--medium">
@@ -108,9 +108,9 @@
                 <el-table-column fixed="right" label="Operations" width="250">
 
                     <template slot-scope="scope">
-
+<!-- 
                         <el-button size="mini" slot="reference" @click="handleEdit(scope.$index, scope.row)">Edit</el-button>
-                        <el-button style="padding-right: 10px" size="mini" type="danger" @click="handleDelete(scope.$index, scope.row)">Delete</el-button>
+                        <el-button style="padding-right: 10px" size="mini" type="danger" @click="handleDelete(scope.$index, scope.row)">Delete</el-button> -->
 
                         <el-button type="success" size="mini" slot="reference" @click="handleApprove(scope.$index, scope.row)"><span>Approve</span></el-button>
                     </template>
@@ -148,13 +148,14 @@
 
         methods: {
             getPolicies(){
-                let url =  'https://capex.wazinsure.com:4443/api/policies';
+                let url =  'https://capex.wazinsure.com:4443/api//pendingpolicies';
                 this.axios.get(url, {headers: {'Content-Type': 'application/json',
                             'Authorization': 'Bearer '+ this.token
                         }}).then((response) => {
                     this.tableData=response.data.data;
 
                      this.excelForm = Object.values(this.tableData);
+                      console.log(this.tableData)
                 });
             },
 
@@ -172,7 +173,7 @@
             },
 
             handleDelete(index, row) {
-                let url = this.url + '/lifepolicies/' + row.life_policy_id;
+                let url = this.url + '/lifepolicies/' + row.policies_id;
                 this.axios.delete(url , {headers: {'Content-Type': 'application/json',
                         'Authorization': 'Bearer '+ this.token
                     }}).then((response)=> {
@@ -187,13 +188,13 @@
 
             handleEdit(index,row){
                 this.form=row;
-                this.id=this.form.life_policy_id;
+                this.id=this.form.policies_id;
                 this.$router.push('/lifepolicies/edit/'+this.id);
             },
 
             handleApprove(index,row){
                 this.form=row;
-                this.id=this.form.life_policy_id;
+                this.id=this.form.policies_id;
                 this.$router.push('/lifepolicies/approve/'+this.id);
             },
 
